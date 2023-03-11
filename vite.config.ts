06701ffa5911564b3c5eb74018ce5dad2e85637f
Josh <37798644+joshuaellis@users.noreply.github.com>
2023-03-11 15:54:17 +0000

    feat: add admin bundling

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  entry: ['admin/src/index.ts'],
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-  minify: !options.watch,
-  outDir: 'dist/admin',
-  format: 'esm',
+  build: {
+    emptyOutDir: false,
+    target: 'esnext',
+    lib: {
+      entry: ['admin/src/index.ts'],
+      name: 'ml-browser',
+      formats: ['cjs', 'es'],
+      fileName(format) {
+        return `[name].${format === 'es' ? 'js' : format}`;
+      },
+    },
+    outDir: 'dist/admin',
+    rollupOptions: {
+      external: (id) => !id.startsWith('.') && !id.startsWith('/'),
+    },
+  },
+  plugins: [react()],
 });
